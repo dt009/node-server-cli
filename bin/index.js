920e94d61dd78019360c625a0d03521286836f71
dt009 <18363625031@163.com>
2018-08-22 11:15:14 +0800

    clone 命令完成

---
+++ bin/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/*
+ * @Author: duantao-ds
+ * @Date: 2018-08-21 19:02:33
+ * @Last Modified by: duantao-ds
+ * @Last Modified time: 2018-08-21 19:03:23
+ */
+
+ require('../lib/cli/index.js');
